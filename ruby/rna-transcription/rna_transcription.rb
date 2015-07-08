56320a0eb2848705ca1cf0697bdaee07f721b232
Max Tedford <max.tedford@gmail.com>
2015-07-07 22:04:22 -0600

    clean up rna-transcription

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,30 +1,28 @@
 class Complement
-  def self.of_dna(dna_strand)
-    dna = {"G" => "C",
-           "C" => "G",
-           "T" => "A",
-           "A" => "U"}
 
+  @dna = {"G" => "C",
+          "C" => "G",
+          "T" => "A",
+          "A" => "U"}
+  
+  @rna = @dna.invert
+  
+  def self.of_dna(dna_strand)
     dna_strand.chars.map do |nucleotide|
-      if dna[nucleotide].nil?
+      if @dna[nucleotide].nil?
         raise(ArgumentError)
       else
-      dna[nucleotide]
+        @dna[nucleotide]
       end
     end.join
   end
 
   def self.of_rna(rna_strand)
-    rna = {"C" => "G",
-           "G" => "C",
-           "A" => "T",
-           "U" => "A"}
-
     rna_strand.chars.map do |nucleotide|
-      if rna[nucleotide].nil?
+      if @rna[nucleotide].nil?
         raise(ArgumentError)
       else
-      rna[nucleotide]
+      @rna[nucleotide]
       end
     end.join
   end
